aa2583e129826d7e32749f00aa0bb5dad27899b8
Nicolle Rosa <75190112+Nicoleta0c@users.noreply.github.com>
2024-12-07 00:40:49 -0400

    Actualizar App.tsx

--- Frontend-Inventario/my-app/src/App.tsx
+++ Frontend-Inventario/my-app/src/App.tsx
@@ -3,6 +3,7 @@ import InventoryManagement from './components/InventoryManagement';
 function App() {
   return (
     <div>
+      // Para mirar componente de la pagina principal 
       <InventoryManagement />
     </div>
   );
